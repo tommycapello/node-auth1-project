d248bed34587e661caf85dfb17bcad64bdd4f9d1
tommycapello <thomcapello@gmail.com>
2021-07-19 16:52:31 -0400

    users model complete

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -17,14 +17,15 @@ function findBy(filter) {
   resolves to the user { user_id, username } with the given user_id
  */
 function findById(user_id) {
-
+  return db('users').where('user_id', user_id).first()
 }
 
 /**
   resolves to the newly inserted user { user_id, username }
  */
-function add(user) {
-
+async function add(user) {
+  const [id] = await db('users').insert(user)
+  return findById(id)
 }
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
